3e3184fbbfcc93f3caa01b2ed18123c86dddbe05
hrxgaurav <magicgalaxy0@gmail.com>
2024-05-28 12:39:54 +0530

    fix-server_Sleep

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
-import http from 'http';
-import cron from 'node-cron';
 import connectDB from './config/connectDB.js';
 import otpRoutes from './routes/otpRoutes.js';
 import authRoutes from './routes/authRoutes.js';
@@ -26,32 +24,6 @@ app.use(express.json());
 app.use(authRoutes);
 app.use(otpRoutes);
 app.use(profileRoutes);
-
-// Pinging the server itself to keep it awake
-const pingServer = async () => {
-    return new Promise((resolve, reject) => {
-        http.get(`http://localhost:${PORT}/ping`, (response) => {
-            let data = '';
-            response.on('data', (chunk) => {
-                data += chunk;
-            });
-            response.on('end', () => {
-                resolve(data);
-            });
-        }).on('error', (error) => {
-            reject(error);
-        });
-    });
-};
-
-cron.schedule('*/5 * * * *', async () => {
-    try {
-        const res = await pingServer();
-        console.log('Pinged the server to keep it awake:', res);
-    } catch (error) {
-        console.error('Error pinging the server:', error.message);
-    }
-});
 
 app.get('/ping', (req, res) => {
     res.send('Server is awake');
